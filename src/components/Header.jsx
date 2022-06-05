d7cfaee69fab7a7c78b55eb6a6820c25ec669013
Chartik <chart2321@gmail.com>
2022-06-05 19:45:53 +0200

    New files

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src='https://img.freepik.com/darmowe-psd/makieta-logo-na-szarej-scianie_35913-2122.jpg'></img>
+      </header>
+    );
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className='content'>
+      <img src='https://klike.net/uploads/posts/2019-05/1556945364_1.jpg' />
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+      </div>
+      <div>
+        New posts
+      </div>
+      <div>
+        Post 1
+        </div>
+      <div>
+        Post 2
+        </div>
+    </div>
+    );
+}
+export default Profile;
